c266d8d89bce9500319bcf7ff8667df530b00713
Harshith Raj Uppula <80810257+harsha954@users.noreply.github.com>
2021-10-26 12:53:57 +0530

    Create sum of all num ranges(15-30).java

---
+++ sum of all num ranges(15-30).java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        int i=15;
+        int sum=0;
+    while(i>=15 && i<30){
+        sum+=i;
+        i+=1;
+    }
+    System.out.println(sum);
+    }
+}
