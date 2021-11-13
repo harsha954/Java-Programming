585f50feb9385364374eb7962e3201a2eabac993
Harshith Raj Uppula <80810257+harsha954@users.noreply.github.com>
2021-11-13 15:49:42 +0530

    Create MultiThreading1.java

---
+++ MultiThreading1.java
@@ -0,0 +1,30 @@
+class A extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hi");
+            try{Thread.sleep(500);}catch(Exception e){}
+            
+        }
+    }
+}
+
+class B extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+            try{Thread.sleep(500);}catch(Exception e){}
+            
+        }
+    }
+}
+
+public class MultiThreading1 {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        try{Thread.sleep(10);}catch(Exception e){}
+        obj2.start();
+    }
+}
